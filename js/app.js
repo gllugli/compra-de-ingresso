39959ba59d3430961deccfb0be8cd63ea99277ea
Gabriel Larsão Lugli <146869089+gllugli@users.noreply.github.com>
2023-10-05 11:13:55 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,48 @@
+function comprar(id) {
+    let tipo = document.getElementById('tipo-ingresso').value; 
+    let qtd = parseInt(document.getElementById('qtd').value);
+    
+    if (tipo == 'pista') {
+        comprarPista(qtd);
+    } else if (tipo == 'superior') {
+        comprarSuperior(qtd);
+    } else {
+        comprarInferior(qtd);
+    }
+
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade Indisponível para Pista.');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade Indisponível para Cadeira Superior.');
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert('Quantidade Indisponível para Cadeira Inferior.');
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+
